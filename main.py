48f9b53f982ff54d1a95afc46e51c550554fc0a4
Evgeny Pekerman <geka.kud@gmail.com>
2024-06-13 15:18:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,49 +40,49 @@ async def dashboard():
 
 cnt = 0
 import random
-# @app.websocket("/ws/{client_id}")
-# async def websocket_endpoint(websocket: WebSocket, client_id: int):
-#     print(f"Client #{client_id} attempting to connect...")
-#     await manager.connect(websocket)
-#     try: 
-#         while True:
-#             data = await websocket.receive_text()
-#             try:
-#                 received_msg = Message.from_json(data) # Assuming data is sent as JSON
-#             except JsonConvertException as e:
-#                 await manager.send_personal_message(f"server::private::Error: {str(e)}", websocket)
-#                 print(f"Error: {str(e)}")
-#                 continue
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    print(f"Client #{client_id} attempting to connect...")
+    await manager.connect(websocket)
+    try: 
+        while True:
+            data = await websocket.receive_text()
+            try:
+                received_msg = Message.from_json(data) # Assuming data is sent as JSON
+            except JsonConvertException as e:
+                await manager.send_personal_message(f"server::private::Error: {str(e)}", websocket)
+                print(f"Error: {str(e)}")
+                continue
 
-#             if received_msg.type == "location":
-#                 # Broadcast this updated message to all clients
-#                 received_msg.latitude += random.uniform(-0.05, 0.05)
-#                 received_msg.longitude += random.uniform(-0.05, 0.05)
-#                 await manager.broadcast(received_msg.to_json(), websocket)
-#                 latitude = received_msg.latitude
-#                 longitude = received_msg.longitude
+            if received_msg.type == "location":
+                # Broadcast this updated message to all clients
+                received_msg.latitude += random.uniform(-0.05, 0.05)
+                received_msg.longitude += random.uniform(-0.05, 0.05)
+                await manager.broadcast(received_msg.to_json(), websocket)
+                latitude = received_msg.latitude
+                longitude = received_msg.longitude
 
-#                 print(f"Received location: {latitude}, {longitude} from client {received_msg.sender_id}")
-#                 server_got_location_msg = Message(type=MessageType.SERVER_MESSAGE, content=f"Server got your location: {str(latitude)}, {str(longitude)}")
-#                 await manager.send_personal_message(server_got_location_msg.to_json(), websocket)
+                print(f"Received location: {latitude}, {longitude} from client {received_msg.sender_id}")
+                server_got_location_msg = Message(type=MessageType.SERVER_MESSAGE, content=f"Server got your location: {str(latitude)}, {str(longitude)}")
+                await manager.send_personal_message(server_got_location_msg.to_json(), websocket)
 
-#             elif received_msg.type == "client_message":
-#                 # Send a private message to the client that sent the message
-#                 print(f"Received message: {received_msg.content} from client {received_msg.sender_id}")
+            elif received_msg.type == "client_message":
+                # Send a private message to the client that sent the message
+                print(f"Received message: {received_msg.content} from client {received_msg.sender_id}")
 
-#                 server_got_msg = Message(type=MessageType.SERVER_MESSAGE, content=f"Server got your message: {received_msg.content}")
-#                 await manager.send_personal_message(server_got_msg.to_json(), websocket)
+                server_got_msg = Message(type=MessageType.SERVER_MESSAGE, content=f"Server got your message: {received_msg.content}")
+                await manager.send_personal_message(server_got_msg.to_json(), websocket)
                 
-#             else:
-#                 # Handle other messages as before
-#                 await manager.send_personal_message(f"server::private::Got your message: {received_msg.to_json()}", websocket)
-#                 await manager.broadcast(f"server::public::Client #{client_id} posted message: {received_msg.to_json()}", websocket)
-#     except WebSocketDisconnect:
-#         manager.disconnect(websocket)
-#         await manager.broadcast(f"Client #{client_id} has left", websocket)
-#     except Exception as e:
-#         manager.disconnect(websocket)
-#         print('General exception ' + str(e))
+            else:
+                # Handle other messages as before
+                await manager.send_personal_message(f"server::private::Got your message: {received_msg.to_json()}", websocket)
+                await manager.broadcast(f"server::public::Client #{client_id} posted message: {received_msg.to_json()}", websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} has left", websocket)
+    except Exception as e:
+        manager.disconnect(websocket)
+        print('General exception ' + str(e))
 
 @app.websocket("/ws/dashboard")
 async def dashboard_websocket(websocket: WebSocket):
